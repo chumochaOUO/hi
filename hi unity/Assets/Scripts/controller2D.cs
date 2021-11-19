f189d0e5974bba6b0410137acf54e88761b34e90
chumochaOUO <43989504+chumochaOUO@users.noreply.github.com>
2021-11-19 15:53:20 +0800

    今天有上課!嗚呼!
    角色控制C#
    Tilemap設定
    BLABLA

---
+++ hi unity/Assets/Scripts/controller2D.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+/// <summary>
+/// 控制器：2D 橫向卷軸版本
+/// </summary>
+
+
+public class controller2D : MonoBehaviour
+{
+    #region 欄位：公開
+    [Header("移動速度"), Range(0, 500)]
+    public float speed = 3.5f;
+    [Header("跳躍高度"), Range(0, 1500)]
+    public float jump = 300f;
+    //剛體元件
+    private Rigidbody2D rig;
+
+    #endregion
+    //void = 呼叫
+    //Start 只會執行一次
+    private void Start()
+    {
+        //剛體欄位 = 取得元件<2D 剛體>()
+         rig = GetComponent<Rigidbody2D>(); 
+    }
+
+    ///<summary>
+    ///Update 約 60 FPS → 每一幀都執行1次
+    ///固定更新事件(FixedUpdate)：50 FPS
+    ///處理物理行為
+    ///</summary>
+    private void FixedUpdate()
+    {
+        Move(); 
+    }
+
+
+    #region 方法
+    ///<summary>
+    ///1.玩家是否有按移動按鍵，左右方向鍵或A、D
+    ///2.物件移動行為(API)
+    ///3.Unity API >unityengine>classic>input>getAxis
+    ///</summary>
+    private void Move()
+    {
+        //h 值 指定為 輸入.取得軸向(水平軸) - 水平軸代表左右鍵與 AD
+        float h = Input.GetAxis("Horizontal");
+        print("玩家左右按鍵值" + h); 
+    }
+    #endregion
+}
